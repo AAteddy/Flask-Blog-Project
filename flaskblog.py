3a1a3b2edcc35466235ac2165e9b72e1fcf2a119
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-25 15:59:09 +0300

    home.html & about.html templates created

--- flaskblog.py
+++ flaskblog.py
@@ -2,16 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Blog Post 1",
+        "content": "This is the content of blog post 1",
+        "date_posted": "April 24, 2021",
+    },
+    {
+        "author": "Calvert Lowe",
+        "title": "Blog Post 2",
+        "content": "This is the content of blog post 2",
+        "date_posted": "May 15, 2021",
+    },
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
