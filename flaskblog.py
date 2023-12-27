f9f926315def19b81ec5fa7a1ae5e31e859a1f86
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-27 18:10:01 +0300

    flask-sqlalchemy installed for storage

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,13 @@
 from flask import Flask, flash, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "d346f2dd7246a059cc2b0d221c1f1200"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
 
 posts = [
     {
