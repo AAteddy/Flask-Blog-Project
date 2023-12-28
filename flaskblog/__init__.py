aca39fce939ffef6f40ee92f88c595c6cb4de6ea
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-28 10:19:22 +0300

    __init__.py imported routes below the app variable

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,3 +6,5 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "d346f2dd7246a059cc2b0d221c1f1200"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
+
+from flaskblog import routes
