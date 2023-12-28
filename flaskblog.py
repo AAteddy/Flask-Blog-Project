ad39efd457563d586abf69b70ed997af95bd52b5
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-28 09:46:14 +0300

    circular import error issue

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,14 @@
-from datetime import datetime
 from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from models import User, Post
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "d346f2dd7246a059cc2b0d221c1f1200"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
+from models import User, Post
 
 posts = [
     {
