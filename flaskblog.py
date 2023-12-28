21664308af8a125d85c3a19c5f61a966fb564b0e
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-28 09:07:52 +0300

    flaskblog.py file updated

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from flask import Flask, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-from datetime import datetime
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "d346f2dd7246a059cc2b0d221c1f1200"
