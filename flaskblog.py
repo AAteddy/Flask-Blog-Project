60937cbb496f16f0dd9a95a91cff3acb6f0a472c
Tedros Alem <tedsaasfaha@gmail.com>
2023-12-28 09:54:11 +0300

    __init__.py created for initializing all imports

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,3 @@
-from flask import Flask, flash, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "d346f2dd7246a059cc2b0d221c1f1200"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-db = SQLAlchemy(app)
-
 from models import User, Post
 
 posts = [
